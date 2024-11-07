c35b3536870ca89c426131c4ac03ffb08271cc94
Mudasir Bashir <for.all6474@gmail.com>
2024-11-07 11:44:00 +0500

    Hello world program

---
+++ basics_of_kotlin/1.FirstProgram/helloWorld.kt
@@ -0,0 +1,7 @@
+fun main(){
+    println("Hello world!!!")
+}
+
+// "fun" is function definition
+// "main" is function name we can define any
+// "println" print a text in a line and create a new line means it takes entire line 
